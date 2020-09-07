83b3358267c41b7cfd565ad9bd5e78d1968cdb91
wujekbobixon <33070347+wujekbobixon@users.noreply.github.com>
2020-09-07 23:47:39 +0200

    Add files via upload

---
+++ AI.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DobreAI : MonoBehaviour {
+
+	
+	public float predkoscObrotu = 6.0f;
+ 	public bool gladkiObrot = true;
+	public float predkoscRuchu = 5.0f; 
+	public float rangelook = 30f;
+	public float odstepOdGracza = 2f;
+
+	private Transform mojObiekt; 
+	private Transform gracz;
+	private bool patrzNaGracza = false;
+	private Vector3 pozycjaGraczaXYZ; 
+
+	// Use this for initialization
+	void Start () {
+		mojObiekt = transform; 
+		if (GetComponent<Rigidbody> ()) {
+			GetComponent<Rigidbody> ().freezeRotation = true;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		gracz = GameObject.FindWithTag("Player").transform; 
+		pozycjaGraczaXYZ = new Vector3(gracz.position.x, mojObiekt.position.y, gracz.position.z); // Pobieranie XYZ z gracza
+		float dist = Vector3.Distance (mojObiekt.position, gracz.position); // zmienna.distance =(x-y)
+
+		patrzNaGracza = false; 
+		if(dist <= rangelook && dist > odstepOdGracza) {
+			patrzNaGracza = true;
+			mojObiekt.position = Vector3.MoveTowards(mojObiekt.position, pozycjaGraczaXYZ, predkoscRuchu * Time.deltaTime);//zmienna.MoveTowards aktualizuje nowa pozycje
+
+		} else if(dist <= odstepOdGracza) { 
+			patrzNaGracza = true;
+		}
+
+		patrzNaMnie ();
+	}
+
+	void patrzNaMnie(){
+		if (gladkiObrot && patrzNaGracza == true){
+
+			//Quaternion.LookRotation - zwraca quaternion na podstawie werktora kierunku/pozycji
+			Quaternion rotation = Quaternion.LookRotation(pozycjaGraczaXYZ - mojObiekt.position);
+			mojObiekt.rotation = Quaternion.Slerp(mojObiekt.rotation, rotation, Time.deltaTime * predkoscObrotu);
+		} else if(!gladkiObrot && patrzNaGracza == true){ 
+		
+			//Błyskawiczny obrót wroga.
+			transform.LookAt(pozycjaGraczaXYZ);
+		}
+
+	}
+}
---
+++ chodzenie.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chodzenie : MonoBehaviour
+{
+    public CharacterController characterControler;
+        public float speed = 9.0f;
+        public float heightjump = 7.0f;
+        public float nowheightjump = 0.0f;
+        public float running = 7.0f;
+	public float czuloscMyszki = 3.0f;
+	public float myszGoraDol = 0.0f;
+	//Zakres patrzenia w górę i dół.
+	public float zakresMyszyGoraDol = 90.0f;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        characterControler = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        klawiatura();
+        myszka();
+    }
+	private void klawiatura()
+	{
+		// jeżeli wartość dodatnia to poruszamy się do przodu,
+		// jeżeli wartość ujemna to poruszamy się do tyłu.
+		float rochPrzodTyl = Input.GetAxis("Vertical") * speed;
+		//Pobranie prędkości poruszania się lewo/prawo.
+		// jeżeli wartość dodatnia to poruszamy się w prawo,
+		// jeżeli wartość ujemna to poruszamy się w lewo.
+		float rochLewoPrawo = Input.GetAxis("Horizontal") * speed;
+		//Debug.Log (rochLewoPrawo);
+
+		//Skakanie
+		if (characterControler.isGrounded && Input.GetButton("Jump"))
+		{
+			nowheightjump = heightjump;
+		}
+		else if (!characterControler.isGrounded) { 
+			nowheightjump += Physics.gravity.y * Time.deltaTime;
+		}
+
+		//Debug.Log(Physics.gravity.y);
+
+		//Bieganie
+		if (Input.GetKeyDown("left shift"))
+		{
+			speed += running;
+		}
+		else if (Input.GetKeyUp("left shift"))
+		{
+			speed -= running;
+		}
+
+		Vector3 ruch = new Vector3(rochLewoPrawo, nowheightjump, rochPrzodTyl);
+		ruch = transform.rotation * ruch;
+
+		characterControler.Move(ruch * Time.deltaTime);
+	}
+
+
+	private void myszka()
+	{
+		// jeżeli wartość dodatnia to poruszamy w prawo,
+		// jeżeli wartość ujemna to poruszamy w lewo.
+		float myszLewoPrawo = Input.GetAxis("Mouse X") * czuloscMyszki;
+		transform.Rotate(0, myszLewoPrawo, 0);
+
+		// jeżeli wartość dodatnia to poruszamy w górę,
+		// jeżeli wartość ujemna to poruszamy w dół.
+		myszGoraDol -= Input.GetAxis("Mouse Y") * czuloscMyszki;
+
+		//Funkcja nie pozwala aby wartość przekroczyła dane zakresy.
+		myszGoraDol = Mathf.Clamp(myszGoraDol, -zakresMyszyGoraDol, zakresMyszyGoraDol);
+	//Obrucenie kamery a nie characyterem
+		Camera.main.transform.localRotation = Quaternion.Euler(myszGoraDol, 0, 0);
+	}
+}
